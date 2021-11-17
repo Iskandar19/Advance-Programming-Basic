047f009152aa235e27cabe0cc2ba7053fe33f901
Iskandar <walhamdulillah158@gmail.com>
2021-11-17 15:59:10 +0800

    Algorithms and Files

---
+++ CH4/example11 (mod).py
@@ -0,0 +1,19 @@
+# Read a particular line from a file. User provides both the line
+# number and file name
+
+file_str = input("Open what file : ")
+find_line_str = input("Which line (integer) : ")
+
+input_file = open(file_str)		#potential user error
+find_line_int = int(find_line_str)	#potential user error
+line_count_int = 1
+for line_str in input_file:
+	if line_count_int == find_line_int:
+		print("Line{} of file {} is {}".format(find_line_int, file_str,line_str))
+		break
+	line_count_int += 1
+else:
+	# Go here if line sough doesn't exist
+	print("Line {} of file {} not found".format(find_line_int, file_str))
+input_file.close()
+
